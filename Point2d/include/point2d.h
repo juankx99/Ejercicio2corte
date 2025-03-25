e8fac356e741a04ad787ff2645ce8c32b945394c
Juan <juan.cadena-m@mail.escuelaing.edu.co>
2025-03-25 14:09:02 -0500

    se crea .h de point2d

---
+++ Point2d/include/point2d.h
@@ -0,0 +1,33 @@
+#ifndef POINT2D_H
+#define POINT2D_H
+#include <cmath>
+class Point2d {
+    private:
+        double x;
+        double y;
+    public:
+    // Constructor
+        Point2d();
+        Point2d(double x, double y);
+        Point2d(const Point2d& other);
+    //getters
+        double getX() const;
+        double getY() const;
+    //setters
+        void setX(double x);
+        void setY(double y);
+    //operadores sobrecargados
+        Point2d operator+(const Point2d& other) const;
+        Point2d operator-(const Point2d& other) const;
+        Point2d operator*(double scalar) const;
+        Point2d operator/(double scalar) const;
+        bool operator==(const Point2d& other) const;
+    //metodos adicionales
+    double distanceto(const Point2d& other) const;
+    double distanceToOrigin() const;
+    Point2d midpoint(const Point2d& other) const;
+    Point2d unitvector() const;
+};
+
+#endif // POINT2D_H
+
